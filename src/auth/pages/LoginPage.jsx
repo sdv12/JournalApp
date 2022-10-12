dcc4b4c298d687d64e2d6dc1d2cd75d867908c80
Sergio Villegas <SDVillegas@teco.com.ar>
2022-10-12 15:04:07 -0300

    Cambios en implementaciones

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,27 +1,12 @@
 import { Grid, TextField, Typography, Button, Link } from '@mui/material'
 import { Google } from '@mui/icons-material'
 import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const LoginPage = () => {
   return (
-    <Grid
-        container
-        spacing={ 0 }
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-        >
-
-        <Grid item
-            className='box-shadow'
-            xs={ 3 }
-            sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-            >
-
-                <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
-
-                <form>
+    <AuthLayout title='Login'>
+        <form>
                     <Grid container>
                         <Grid item xs={ 12 } sx={{ mt: 2 }}>
                             <TextField 
@@ -67,10 +52,8 @@ export const LoginPage = () => {
 
                     </Grid>
                 </form>
+    </AuthLayout>
 
-            </Grid>
-
-    </Grid>
   )
 }
 
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,49 @@
-
+import { Grid, TextField, Typography, Button, Link } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+        <form>
+                    <Grid container>
+                        <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                            <TextField 
+                            label='Nombre Completo' 
+                            type='text' 
+                            placeholder='Nombre Completo' 
+                            fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                            <TextField 
+                            label='Contraseña' 
+                            type='password' 
+                            placeholder='contraseña' 
+                            fullWidth
+                            />
+                        </Grid>
+
+                        <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                            <Grid item xs={ 12 } sm={ 6 }>
+                                <Button variant='contained' fullWidth>
+                                    Crear Cuenta
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        
+                        <Grid container direction='row' justifyContent='end'>
+                            <Typography sx={{ mr: 1 }}>Tenes cuenta?</Typography>
+                            <Link component={ RouterLink } color='inherit' to="/auth/login">
+                            Crear una cuenta
+                            </Link>
+                            
+                        </Grid>
+
+                    </Grid>
+                </form>
+    </AuthLayout>
+
   )
 }
-
