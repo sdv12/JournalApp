5f0b94ec130a6fb4b640c0eed601c4b16f7d5ab9
Sergio Villegas <SDVillegas@teco.com.ar>
2022-10-13 12:05:37 -0300

    Implementacion de NavBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,18 @@
-import { MailOutline } from "@mui/icons-material"
+
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPage</Typography>
-    <MailOutline />
-    </>
+    <JournalLayout>
+    
+    <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor saepe temporibus sed pariatur quas laboriosam vitae unde recusandae sequi assumenda tenetur, nam rerum nemo, veritatis aspernatur est blanditiis sit officia.</Typography>
+
+    {/*NothinSelecterd*/}
+    {/*NoteView*/}
+
+    </JournalLayout>
     
   )
 }
