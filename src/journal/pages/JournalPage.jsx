6b262d013541542d3c416370a339f8a8df9bca77
Sergio Villegas <SDVillegas@teco.com.ar>
2022-10-13 17:19:24 -0300

    Añadido de boton y galeria de imagenes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,33 @@
 
-import { Typography } from "@mui/material"
+
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
     
-    <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor saepe temporibus sed pariatur quas laboriosam vitae unde recusandae sequi assumenda tenetur, nam rerum nemo, veritatis aspernatur est blanditiis sit officia.</Typography>
+    {/*<Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor saepe temporibus sed pariatur quas laboriosam vitae unde recusandae sequi assumenda tenetur, nam rerum nemo, veritatis aspernatur est blanditiis sit officia.</Typography>*/}
 
-    {/*NothinSelecterd*/}
+    <NothingSelectedView />
     {/*NoteView*/}
+
+    <IconButton
+      size='large'
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
 
     </JournalLayout>
     
